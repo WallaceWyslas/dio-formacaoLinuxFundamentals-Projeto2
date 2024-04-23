b83d76cb07f04c13cc8e0e02a9b6309ffa71c282
WallaceWyslas <wallacewyslas@gmail.com>
2024-04-23 14:20:22 -0300

    Fixed erros

--- IaC.sh
+++ IaC.sh
@@ -4,28 +4,28 @@
 
 cd /
 
-echo "||Atualizando servidor||"
-sudo apt update #verificando atualizações
-sudo apt upgrade -y #baixando e instalando atualizações
+echo "Atualizando servidor"
+sudo apt-get update #verificando atualizações
+sudo apt-get upgrade -y #baixando e instalando atualizações
 echo "Servidor atualizado"
 echo " "
 
-echo "||Instalando apache2||"
-sudo apt install apache2 -y #instalando apache2
+echo "Instalando apache2"
+sudo apt-get install apache2 -y #instalando apache2
 echo " "
 
-echo "||Instalando unzip||"
-sudo apt install unzip -y #instalando unzip
+echo "Instalando unzip"
+sudo apt-get install unzip -y #instalando unzip
 echo " "
 
-echo "||Baixando repositório||"
+echo "Baixando repositório"
 cd /tmp
-sudo wget -iv https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip -P /tmp #baixando o arquivo para a tmp folder
-sudo unzip /tmp/main.zip #extraindo o arquivo baixado
+sudo wget -iv https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip #baixando o arquivo
+sudo unzip main.zip #extraindo o arquivo baixado
 echo " "
 
-echo "||Clonando repositório||"
+echo "Clonando repositório"
 sudo cp -fR /tmp/linux-site-dio-main/* /var/www/html #copiando para a exibição html do apache
 echo " "
 
-echo "||Script finalizado||"
+echo "Script finalizado"
